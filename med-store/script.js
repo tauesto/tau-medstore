d26c9d5d6096d96c725d9db88a6abac8d1d9112f
tau <subhodas2484@gmail.com>
2026-04-29 15:59:45 +0530

    Add files via upload

---
+++ med-store/script.js
@@ -0,0 +1,42 @@
+const products = [
+  { name: "Paracetamol", price: 20 },
+  { name: "Cough Syrup", price: 80 },
+  { name: "Vitamin C", price: 50 },
+];
+
+let cart = [];
+
+const productDiv = document.getElementById("products");
+
+products.forEach((p, i) => {
+  productDiv.innerHTML += `
+    <div class="card">
+      <h4>${p.name}</h4>
+      <p>₹${p.price}</p>
+      <button onclick="addToCart(${i})">Add</button>
+    </div>
+  `;
+});
+
+function addToCart(i) {
+  cart.push(products[i]);
+  document.getElementById("count").innerText = cart.length;
+}
+
+function openCart() {
+  const cartBox = document.getElementById("cart");
+  const list = document.getElementById("cartItems");
+
+  cartBox.style.display = "block";
+  list.innerHTML = "";
+
+  let message = "Hello, I want to order:\n";
+
+  cart.forEach(item => {
+    list.innerHTML += `<li>${item.name}</li>`;
+    message += item.name + "\n";
+  });
+
+  document.getElementById("orderBtn").href =
+    "https://wa.me/91XXXXXXXXXX?text=" + encodeURIComponent(message);
+}
